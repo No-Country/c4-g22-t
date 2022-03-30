72c5c56f7ab12f17df621669ede053316defdd87
lnlindao <lnlindao@hotmail.com>
2022-03-29 21:03:20 -0500

    Update product card v1

--- Front-e-commerce/src/App.js
+++ Front-e-commerce/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import FeatureProduct from './components/Product/featureProduct'
 import Container from '@mui/material/Container';
 
+
 function App() {
   return (
-    <Container maxWidth="xl css-10m0cap">
+    
+    <Container maxWidth="lg">
         <FeatureProduct/>
       </Container>
   );
