bd44ccab048ffc15c2382ac81c5c45bd80b816fc
Richard Miguel Irala <richardirala47@hotmail.com>
2022-04-01 15:50:06 -0300

    Trending product component y WSO component han sido agregados, junto a ellos se agrego la carpeta imgs

--- Front-e-commerce/src/App.js
+++ Front-e-commerce/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar/Navbar';
+import AddWSO from "./components/W-S-O/index.js";
+import TrendingProduct from "./components/Trending-product/index.js";
+import "./components/W-S-O/index.css";
+import "./components/Trending-product/index.css";
 import {BrowserRouter, Routes, Route}  from 'react-router-dom';
 
 
@@ -13,6 +17,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Homepage/>}/>
             </Routes>
+            <AddWSO/>
+            <TrendingProduct/>
           </BrowserRouter>
 
       </>
