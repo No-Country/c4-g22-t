5084c50f03713a39ee13f5e0d22e37707b8ef851
lnlindao <lnlindao@hotmail.com>
2022-04-07 12:39:32 -0500

    Cart v1

--- Front-e-commerce/src/App.js
+++ Front-e-commerce/src/App.js
@@ -7,21 +7,26 @@ import ProductDetail from './pages/ProductDetail';
 import AddNavbar from './components/headerTemporal';
 import './components/headerTemporal/index.css';
 import PageOrderCompleted from './pages/OrderCompleted';
+import Cart from './components/Cart/Cart';
+import {CartProvider} from './components/context/CartContext'
 
 
 function App() {
   return (
       <>
 
-          <BrowserRouter>
-            <AddNavbar/>
-            <Routes>
-              <Route path="/" element={<Homepage/>}/>
-              <Route path="/order%20completed" element={<PageOrderCompleted/>}/>
-              <Route path="/tienda" element={<Store/>}/>
-              <Route path="/producto/:category/:id" element={<ProductDetail/>}/>
-            </Routes>
-          </BrowserRouter>
+          <CartProvider>
+            <BrowserRouter>
+              <AddNavbar/>
+              <Routes>
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="/order%20completed" element={<PageOrderCompleted/>}/>
+                <Route path="/tienda" element={<Store/>}/>
+                <Route path="/producto/:category/:id" element={<ProductDetail/>}/>
+                <Route path="/cart" element={<Cart/>}/>
+              </Routes>
+            </BrowserRouter>
+          </CartProvider>
           
   
 
