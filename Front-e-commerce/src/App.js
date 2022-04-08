f7ae1d56121d6440e388a874e8a60d9abb082d2d
Richard Miguel Irala <richardirala47@hotmail.com>
2022-04-08 03:03:20 -0300

    componente form login creado, archivo JS para la page my account creada y App js modificado

--- Front-e-commerce/src/App.js
+++ Front-e-commerce/src/App.js
@@ -8,7 +8,8 @@ import AddNavbar from './components/headerTemporal';
 import './components/headerTemporal/index.css';
 import PageOrderCompleted from './pages/OrderCompleted';
 import Cart from './components/Cart/Cart';
-import {CartProvider} from './components/context/CartContext'
+import {CartProvider} from './components/context/CartContext';
+import PageMyAccount from './pages/MyAccount';
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
                 <Route path="/tienda" element={<Store/>}/>
                 <Route path="/producto/:category/:id" element={<ProductDetail/>}/>
                 <Route path="/cart" element={<Cart/>}/>
+                <Route path="/my%20account" element={<PageMyAccount/>}/>
               </Routes>
             </BrowserRouter>
           </CartProvider>
