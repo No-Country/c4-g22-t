c28eb92c3b71d8be6ebbaa7851f3235eb278232f
Richard Miguel Irala <richardirala47@hotmail.com>
2022-04-14 13:38:50 -0300

    Se agrego el componente register y se lo utilizo en la nueva page de Registro

---
+++ Front-e-commerce/src/components/form-register/AddFormRegister.jsx
@@ -0,0 +1,60 @@
+import "./AddFormRegister.css";
+function AddFormRegister() {
+  return (
+    <div className="afl-container">
+      <form className="form-login-container">
+        <div className="full-width">
+          <h2 className="form-title text-align-center">Register</h2>
+          <h3 className="form-body text-align-center">
+            Please register using account detail bellow.
+          </h3>
+        </div>
+        <div className="full-width form-inputs-container">
+          <label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Enter your first name"
+            />
+          </label>
+          <label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Enter your last name"
+            />
+          </label>
+          <label>
+            <input
+              className="form-input"
+              type="email"
+              placeholder="Email Address"
+            />
+          </label>
+          <label>
+            <input className="form-input" type="text" placeholder="Password" />
+          </label>
+          <label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder="Repeat Password"
+            />
+          </label>
+        </div>
+        <div className="full-width">
+          <button className="form-login__button--send">Create Account</button>
+        </div>
+        <div className="full-width">
+          <h3 className="form-body text-align-center">
+            You have an Account?{" "}
+            <a className="form-body" href="/my%20account">
+              Log in
+            </a>
+          </h3>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default AddFormRegister;
